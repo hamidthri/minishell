f44442f08ce2bdf6a87bd5059e88a249a8fe1e03
Mohammad Momeni <lo3me@pm.me>
2023-12-19 17:05:20 +0100

    Pipes modificaton

--- system.c
+++ system.c
@@ -6,59 +6,75 @@
 /*   By: mmomeni <mmomeni@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/13 15:16:48 by mmomeni           #+#    #+#             */
-/*   Updated: 2023/11/15 16:48:14 by mmomeni          ###   ########.fr       */
+/*   Updated: 2023/12/18 19:19:10 by mmomeni          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	terminate(char *reason)
-{
-	perror(reason);
-	exit(-1);
-}
-
-int	run(char *s)
+static void	run(char *s)
 {
 	char	*file;
-	int		n;
 	char	**v;
 
 	v = ft_split(s, ' ');
+	if (!v || !*v)
+		return ;
 	if (v[0][0] == '/')
 		file = ft_strdup(v[0]);
 	else
 		file = get_path(v[0]);
-	n = execve(file, v, NULL);
+	if (!file)
+		return (terminate(s, "command not found"));
+	if (execve(file, v, NULL) < 0)
+		terminate(file, NULL);
 	free(file);
-	ft_free_split(v);
-	return (n);
+	ft_vecfree(v);
 }
 
 void	run_pipes(int fd[2], char **commands, int n)
 {
-	pid_t	pid;
-	int		status;
+	int			i;
+	int			pid;
+	int			status;
+	int			in_fd;
+	extern char	**environ;
 
-	while (n-- > 1 && *commands)
+	i = 0;
+	in_fd = 0;
+	while (i < n)
 	{
-		pipe(fd);
+		if (i < n - 1)
+			pipe(fd);
 		pid = fork();
 		if (pid == 0)
 		{
-			dup2(fd[1], 1);
-			close(fd[0]);
-			run(*commands);
-			terminate(*commands);
+			if (in_fd != 0)
+			{
+				dup2(in_fd, 0);
+				close(in_fd);
+			}
+			if (i < n - 1)
+			{
+				close(fd[0]);
+				dup2(fd[1], 1);
+				close(fd[1]);
+			}
+			run(commands[i]);
+			exit(EXIT_FAILURE);
 		}
 		else
 		{
-			dup2(fd[0], 0);
-			close(fd[1]);
+			if (in_fd != 0)
+				close(in_fd);
+			if (i < n - 1)
+			{
+				close(fd[1]);
+				in_fd = fd[0];
+			}
 			waitpid(pid, &status, 0);
+			set_env(&environ, "?", ft_itoa(status));
 		}
-		commands++;
+		i++;
 	}
-	run(*commands);
-	terminate(*commands);
 }
