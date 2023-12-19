05a32acd7ec02d0f0de876eb16cf58c7f13318c9
Mohammad Momeni <lo3me@pm.me>
2023-12-19 17:05:52 +0100

    Signals

--- main.c
+++ main.c
@@ -6,22 +6,92 @@
 /*   By: mmomeni <mmomeni@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/15 16:14:02 by mmomeni           #+#    #+#             */
-/*   Updated: 2023/12/02 19:37:18 by mmomeni          ###   ########.fr       */
+/*   Updated: 2023/12/18 16:05:20 by mmomeni          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	main(int argc, char **argv)
+int	h_up(int count, int key)
 {
-    char * line;
-    int fd[2];
+	HIST_ENTRY	*entry;
 
-    line = get_next_line(STDIN_FILENO);
-    while (line)
-    {
-        run_pipes(fd, argv, argc);
-        free(line);
-        line = NULL;
-    }
+	(void)count;
+	(void)key;
+	entry = previous_history();
+	if (entry)
+	{
+		rl_replace_line(entry->line, 0);
+		rl_point = rl_end;
+		rl_redisplay();
+	}
+	return (0);
+}
+
+int	h_down(int count, int key)
+{
+	HIST_ENTRY	*entry;
+
+	(void)count;
+	(void)key;
+	entry = next_history();
+	if (entry)
+		rl_replace_line(entry->line, 0);
+	else
+		rl_replace_line("", 0);
+	rl_point = rl_end;
+	rl_redisplay();
+	return (0);
+}
+
+void	disable_ctrl_sign(void)
+{
+	struct termios	term;
+
+	tcgetattr(0, &term);
+	term.c_lflag &= ~(ECHOCTL);
+	tcsetattr(0, TCSANOW, &term);
+}
+
+void	handle_ctrl_c(int sig)
+{
+	(void)sig;
+	ft_putstr_fd("\n", 1);
+	rl_replace_line("", 0);
+	rl_on_new_line();
+	rl_redisplay();
+}
+
+int	main(void)
+{
+	char		*line;
+	char		**tokens;
+	int			i;
+	extern char	**environ;
+
+	using_history();
+	rl_bind_keyseq("\\e[A", &h_up);
+	rl_bind_keyseq("\\e[B", &h_down);
+	disable_ctrl_sign();
+	signal(SIGINT, handle_ctrl_c);
+	while (1)
+	{
+		line = readline("minishell$ ");
+		if (!line)
+			break ;
+		if (!*line)
+			continue ;
+		add_history(line);
+		tokens = ft_split(line, '|');
+		i = 0;
+		while (tokens[i++])
+			tokens[i - 1] = parse(tokens[i -1], environ);
+		run_pipes((int[2]){1, 2}, tokens, ft_veclen(tokens));
+		free(line);
+		ft_vecfree(tokens);
+		line = NULL;
+		tokens = NULL;
+		write_history("~/.minishell_history");
+	}
+	return (0);
 }
